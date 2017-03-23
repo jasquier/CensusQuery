982e678b7aef9c38a8e7515079092f047cad2142
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-23 11:55:48 -0400

    able to consume the response from the census api using ResponseEntity and then examining the body

--- rest/src/main/java/squier/john/pp/CensusqueryApplication.java
+++ rest/src/main/java/squier/john/pp/CensusqueryApplication.java
@@ -1,23 +1,28 @@
 package squier.john.pp;
 
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-import squier.john.pp.model.Query;
+
+import java.lang.reflect.ParameterizedType;
 
 @SpringBootApplication
-public class CensusqueryApplication {
+public class CensusQueryApplication {
 
-    private static final Logger log = LoggerFactory.getLogger(CensusqueryApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(CensusQueryApplication.class);
+    private final String wMTwentyNineDEURL = "http://api.census.gov/data/2010/sf1?key=b315c1e1cc2b89d6770000ab9d29c68f45162522&get=PCT012A029&for=state:08";
 
 	public static void main(String[] args) {
-		SpringApplication.run(CensusqueryApplication.class, args);
+		SpringApplication.run(CensusQueryApplication.class, args);
 	}
 
     @Bean
@@ -26,12 +31,11 @@ public class CensusqueryApplication {
     }
 
     @Bean
-    public ApplicationRunner run(RestTemplate restTemplate) throws Exception {
-        return (ApplicationArguments args) -> {
-            Query query = restTemplate.getForObject(
-                    "http://api.census.gov/data/2010/sf1?key=b315c1e1cc2b89d6770000ab9d29c68f45162522&get=PCT012A029&for=state:08",
-                    Query.class);
-            log.info(query.toString());
+    public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+        return args -> {
+            ResponseEntity<ArrayNode> entity = restTemplate.exchange(wMTwentyNineDEURL, HttpMethod.GET, null,
+                    new ParameterizedTypeReference<ArrayNode>(){});
+            System.out.println(entity.getBody().toString());//////////////////////////////////////////////////////////////////////
         };
     }
 }
