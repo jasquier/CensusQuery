353b8d8f264dc5b3ee525b323c55592e45217e1c
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-23 09:10:45 -0400

    initial commit, front and middle projects created

---
+++ rest/src/main/java/squier/john/pp/CensusqueryApplication.java
@@ -0,0 +1,12 @@
+package squier.john.pp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CensusqueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CensusqueryApplication.class, args);
+	}
+}
---
+++ rest/src/test/java/squier/john/pp/CensusqueryApplicationTests.java
@@ -0,0 +1,16 @@
+package squier.john.pp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CensusqueryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
