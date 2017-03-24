d8a97fdbcd2e8a6ad6a98f66e509290afbfb48ba
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-24 14:21:07 -0400

    this time

--- rest/src/main/java/squier/john/pp/CensusqueryApplication.java
+++ rest/src/main/java/squier/john/pp/CensusqueryApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CensusQueryApplication {
+public class CensusqueryApplication {
     public static void main(String[] args) {
-        SpringApplication.run(CensusQueryApplication.class, args);
+        SpringApplication.run(CensusqueryApplication.class, args);
     }
 }
