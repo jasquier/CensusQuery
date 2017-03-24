896cd19608f3bd8d51233154c63b3a0abb94c47f
John Squier <johnsquier@zipcoders-MacBook-Pro-3.local>
2017-03-24 14:20:26 -0400

    what is going on with the app file

--- rest/src/main/java/squier/john/pp/CensusqueryApplication.java
+++ rest/src/main/java/squier/john/pp/CensusqueryApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CensusQueryApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(CensusQueryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CensusQueryApplication.class, args);
+    }
 }
